3c6048dafbde290f8280789846a163977db01c24
danhaywood <dan@haywood-associates.co.uk>
2019-10-17 05:07:13 +0200

    disables spi-rest, in favour of spi-cxfrs

--- webapp/src/main/java/org/incode/ixnsubscriber/webapp/InteractionSubscriberApplication.java
+++ webapp/src/main/java/org/incode/ixnsubscriber/webapp/InteractionSubscriberApplication.java
@@ -1,17 +1,11 @@
 package org.incode.ixnsubscriber.webapp;
 
-import org.incode.ixnsubscriber.dispatcher.DispatcherModule;
-import org.incode.ixnsubscriber.spi.log.RelayLogModule;
-import org.incode.ixnsubscriber.spi.rest.RelayRestModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({
-        RelayRestModule.class,
-        RelayLogModule.class,
-        DispatcherModule.class,
         WebappModule.class
 })
 public class InteractionSubscriberApplication {
--- webapp/src/main/java/org/incode/ixnsubscriber/webapp/WebappModule.java
+++ webapp/src/main/java/org/incode/ixnsubscriber/webapp/WebappModule.java
@@ -1,12 +1,21 @@
 package org.incode.ixnsubscriber.webapp;
 
 
+import org.incode.ixnsubscriber.dispatcher.DispatcherModule;
+import org.incode.ixnsubscriber.spi.cxfrs.RelayCxfrsModule;
+import org.incode.ixnsubscriber.spi.log.RelayLogModule;
 import org.incode.ixnsubscriber.webapp.config.AppConfig;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 
 @Configuration
+@Import({
+        RelayLogModule.class,
+        RelayCxfrsModule.class,
+        DispatcherModule.class
+})
 @EnableConfigurationProperties(AppConfig.class)
 public class WebappModule {
 
